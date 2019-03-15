b6483a262531660b4d870ae9d2854385858733b5
pashin <k.pashin@parcsis.org>
2019-03-15 11:51:19 +0400

    additional links

--- front/src/config/table.config.ts
+++ front/src/config/table.config.ts
@@ -2,9 +2,15 @@ interface AdditionalField {
 	fieldName: string;
 }
 
+interface AdditionalLink {
+	name: string;
+	url: string;
+}
+
 export interface FieldConfig {
 	title: string;
 	fieldName: string;
 	type?: string;
 	additionalField?: AdditionalField;
+	additionalLinks?: AdditionalLink[];
 }
