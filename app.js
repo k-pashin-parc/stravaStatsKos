f18a934da19da33e2ac25363101175d5c96ca5cc
pashin <k.pashin@parcsis.org>
2020-01-23 17:48:11 +0400

    sslify

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express'),
 	bodyParser = require( 'body-parser' ),
 	app = express(),
-	mainStatsRoute = require('./routes/mainStatsRoute');
+	mainStatsRoute = require('./routes/mainStatsRoute'),
+	enforce = require('express-sslify');
 
 app
 	.set('port', (process.env.PORT || 5000))
@@ -9,7 +10,8 @@ app
 	.use(bodyParser.urlencoded({
 		extended: true
 	}))
-	.use(express.static(__dirname + '/dist'));
+	.use(express.static(__dirname + '/dist'))
+	.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 mainStatsRoute(app);
 
